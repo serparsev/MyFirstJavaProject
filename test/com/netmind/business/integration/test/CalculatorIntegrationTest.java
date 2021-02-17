2c1015766a8149af922ce71f0ce88f58bdaa3cee
Sergio Parra Sevillano <serparsev@gmail.com>
2021-02-17 19:26:33 +0100

    Calculator integration test implemented.

--- test/com/netmind/business/integration/test/CalculatorIntegrationTest.java
+++ test/com/netmind/business/integration/test/CalculatorIntegrationTest.java
@@ -16,17 +16,20 @@ public class CalculatorIntegrationTest {
 
 	@Test
 	public void testSubstract() {
-		fail("Not yet implemented");
+		Calculator calculator = new Calculator();
+		assertEquals(calculator.substract(4, 2),2);
 	}
 
 	@Test
 	public void testMultiply() {
-		fail("Not yet implemented");
+		Calculator calculator = new Calculator();
+		assertEquals(calculator.multiply(4, 2),8);
 	}
 
 	@Test
 	public void testDivide() {
-		fail("Not yet implemented");
+		Calculator calculator = new Calculator();
+		assertEquals(calculator.divide(4, 2),2);
 	}
 
 }
