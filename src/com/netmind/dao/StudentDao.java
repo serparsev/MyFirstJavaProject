109878250db742ad3c12268f97084c8a256af72e
Sergio Parra Sevillano <serparsev@gmail.com>
2021-02-25 12:37:33 +0100

    Created writeCSV method in FileManagerDao

--- src/com/netmind/dao/StudentDao.java
+++ src/com/netmind/dao/StudentDao.java
@@ -1,40 +1,25 @@
 package com.netmind.dao;
 
-import java.io.File;
-import java.io.IOException;
+import static com.netmind.dao.FileManagerDao.createFile;
+import static com.netmind.dao.FileManagerDao.writeCSV;
+
 import java.util.ArrayList;
 
 import com.netmind.model.Student;
 
 public class StudentDao {
 
-	private static final String FILE_PATH = "C:\\Users\\serpa\\eclipse-workspace\\testFile.txt";
-
 	private static ArrayList<Student> studentList = null;
 
 	static {
 		studentList = new ArrayList<Student>();
+		createFile();
 
-	}
-
-	public StudentDao() {
-		File file = new File(FILE_PATH);
-
-		// Create the file
-		try {
-			if (file.createNewFile()) {
-				System.out.println("File is created!");
-			} else {
-				System.out.println("File already exists.");
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 
 	public boolean add(Student student) {
 		studentList.add(student);
+		writeCSV(student);
 		return true;
 
 	}
