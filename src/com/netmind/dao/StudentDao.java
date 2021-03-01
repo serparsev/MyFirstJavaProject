e06a6294a1059eb5644a2e46f0c0b03f826ede1e
Sergio Parra Sevillano <serparsev@gmail.com>
2021-03-01 11:49:54 +0100

    Refactored Dao layer #5

--- src/com/netmind/dao/StudentDao.java
+++ src/com/netmind/dao/StudentDao.java
@@ -1,8 +1,8 @@
 package com.netmind.dao;
 
-import static com.netmind.dao.FileManagerDao.createFile;
-import static com.netmind.dao.FileManagerDao.writeCSV;
-
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import com.netmind.model.Student;
@@ -13,14 +13,24 @@ public class StudentDao {
 
 	static {
 		studentList = new ArrayList<Student>();
-		createFile();
-
 	}
 
 	public boolean add(Student student) {
 		studentList.add(student);
-		writeCSV(student);
-		return true;
 
+		return true;
+	}
+
+	public boolean addStudentToFile(Student student) throws IOException {
+		try (FileWriter fileWriter = new FileWriter(
+				FileManagerDao.getFileName(), true);
+				BufferedWriter bufferWriter = new BufferedWriter(fileWriter)) {
+			bufferWriter.write(student.toTxtFile());
+			bufferWriter.write(System.lineSeparator());
+		} catch (IOException e) {
+			throw e;
+		}
+
+		return true;
 	}
 }
